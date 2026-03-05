4532a3720ec6d94d4e721135a19c9509f3183a18
jy4education_git <jy4education@gmail.com>
2026-03-05 12:30:14 +0100

    +

--- 03_user_DB/03a_db_userDB_SQL.sql
+++ 03_user_DB/03a_db_userDB_SQL.sql
@@ -4,6 +4,13 @@
 /* Table users */
 DROP TABLE IF EXISTS boo.users;
 
+CREATE TABLE IF NOT EXISTS boo.users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    userName VARCHAR(50) NOT NULL UNIQUE,
+    familyName VARCHAR(50) NOT NULL,
+    firstName VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL DEFAULT 'TBA'
+);
 
 /* Struktur */
 DESCRIBE boo.users;
