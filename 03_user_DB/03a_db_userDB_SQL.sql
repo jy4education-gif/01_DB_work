a78474e5542693e57ba0a79f121f8f93c201b351
jy4education_git <jy4education@gmail.com>
2026-03-05 12:49:11 +0100

    +

--- 03_user_DB/03a_db_userDB_SQL.sql
+++ 03_user_DB/03a_db_userDB_SQL.sql
@@ -17,8 +17,17 @@ DESCRIBE boo.users;
 
 
 /* Daten */
+INSERT INTO boo.users(userName,familyName,firstName) 
+VALUES ('johndoe123', 'Doe', 'John');
 
+INSERT INTO boo.users(userName,familyName,firstName) 
+VALUES ('max123', 'Max', 'Moritz');
 
+INSERT INTO boo.users(userName,familyName,firstName) 
+VALUES ('Sybille_die_Stille', 'Sybille', 'Stille');
+
+-- INSERT INTO boo.users(userName,familyName,firstName) 
+-- VALUES ('johndoe123', 'Max', 'Moritz')
 
 /* Inhalte : Ergebnistabelle */
 SELECT * FROM boo.users;
---
+++ 03_user_DB/03b_db_userDB_SQL.sql
@@ -0,0 +1,17 @@
+\! cls
+/* USER TAB. Version 2 */
+
+/* Table users */
+DROP TABLE IF EXISTS boo.users;
+
+/* User Passwort mit varchar 40 erzeugen mit NOT NULL */
+
+/* Struktur */
+DESCRIBE boo.users;
+
+/* Daten */
+
+/* Inhalte : Ergebnistabelle */
+SELECT * FROM boo.users;
+
+
