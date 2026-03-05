1fbee6e56a7277c46cfddf335ddf25327b5c1b52
jy4education_git <jy4education@gmail.com>
2026-03-05 12:20:50 +0100

    add 03_user_DB

---
+++ 03_user_DB/03a_db_userDB_SQL.sql
@@ -0,0 +1,19 @@
+\! cls
+/* USER TAB. Version 1 */
+
+/* Table users */
+DROP TABLE IF EXISTS boo.users;
+
+
+/* Struktur */
+DESCRIBE boo.users;
+
+
+/* Daten */
+
+
+
+/* Inhalte : Ergebnistabelle */
+SELECT * FROM boo.users;
+
+
