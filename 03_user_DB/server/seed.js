511fc252496073740895e1d3563b18530765c093
jy4education_git <jy4education@gmail.com>
2026-03-06 19:27:14 +0100

    added og castmember

--- 03_user_DB/server/seed.js
+++ 03_user_DB/server/seed.js
@@ -3,9 +3,9 @@ import { registerUser } from "./auth.js";
 async function seed() {
     try {
         console.log("Starte Seeding...");
-        await registerUser("Max", "max@example.com", "meinPasswort123");
-        await registerUser("Erika", "erika@example.com", "sonnenschein88");
-        await registerUser("Bob", "bob@example.com", "geheimnis!");
+        await registerUser("Grizabella", "grizabella@westend1981.com", "meinPasswort123");
+        await registerUser("Rum Rum Tugger", "rum_rum_tugger@westend1981.com", "sonnenschein88");
+        await registerUser("Old Deuteronomy", "old_deuteronomy@westend1981.com", "geheimnis!");
         console.log("✅ Drei Testnutzer erfolgreich angelegt!");
         process.exit(0);
     } catch (error) {
