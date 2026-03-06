46b996591d454ee19d003ef899c0098e7ce1f7a2
jy4education_git <jy4education@gmail.com>
2026-03-06 08:46:40 +0100

    add vers doz

---
+++ 03_user_DB/03b_db_userDB_SQL_vers_dozent.sql
@@ -0,0 +1,18 @@
+\! cls
+/* USER TAB. Version 2 */
+
+/* Table users */
+DROP TABLE IF EXISTS boo.users;
+
+-- CREATE ...
+-- userPwd / varchar 40 / not null
+
+
+/* Struktur */
+DESCRIBE boo.users;
+
+/* Daten */
+-- pwd ergänzen
+
+/* Inhalte : Ergebnistabelle */
+SELECT * FROM boo.users;
