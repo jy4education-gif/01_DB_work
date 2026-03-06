ad709fa3d5c2457294b6c921121bb72c33d2c370
jy4education_git <jy4education@gmail.com>
2026-03-06 19:31:31 +0100

    +

--- 03_user_DB/server/seed.js
+++ 03_user_DB/server/seed.js
@@ -3,9 +3,9 @@ import { registerUser } from "./auth.js";
 async function seed() {
     try {
         console.log("Starte Seeding...");
-        await registerUser("Grizabella", "grizabella@westend1981.com", "meinPasswort123");
-        await registerUser("Rum Rum Tugger", "rum_rum_tugger@westend1981.com", "sonnenschein88");
-        await registerUser("Old Deuteronomy", "old_deuteronomy@westend1981.com", "geheimnis!");
+        await registerUser("Elaine Paige", "grizabella@westend1981.com", "meinPasswort123");
+        await registerUser("Paul Nicholas", "rum_rum_tugger@westend1981.com", "sonnenschein88");
+        await registerUser("Brian Blessed", "old_deuteronomy@westend1981.com", "geheimnis!");
         console.log("✅ Drei Testnutzer erfolgreich angelegt!");
         process.exit(0);
     } catch (error) {
